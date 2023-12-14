32189f2cc79c6a3e95957f9e38f350f1047f9670
Juan Chaher <juanchaher99@gmail.com>
2023-12-13 21:58:38 -0300

    move scrapper into a service and have it store page context in journey table

--- services/scrapper/onboardNewPage.ts
+++ services/scrapper/onboardNewPage.ts
@@ -1,11 +1,11 @@
 import puppeteer from 'puppeteer';
 import { Request, Response } from 'express';
 import jsdom from 'jsdom';
-import { DOMHelper, websitesToTest } from '../utils';
-import { MainElements } from '../types';
-import { IElement, IVariant } from '../types';
-import db from '../models';
-import getTextVariants, { generateAiTextResponse } from './getTextVariants';
+import { DOMHelper, websitesToTest } from '../../utils';
+import { MainElements } from '../../types';
+import { IElement, IVariant } from '../../types';
+import db from '../../models';
+import getTextVariants, { generateAiTextResponse } from '../getTextVariants';
 
 async function initiatePage(website) {
   const browser = await puppeteer.launch();
@@ -19,25 +19,25 @@ async function initiatePage(website) {
 }
 
 async function scrapMainElements(
-  website_url: string,
-  close_browser: boolean = true,
+  websiteUrl: string,
+  browserSession: any
+
 ): Promise<MainElements | false> {
-  const { page, browser, statusCode, window } = await initiatePage(website_url);
-  if (statusCode !== 200) return false;
-  const domHelper = DOMHelper(page, window);
+  console.log("website url: ", websiteUrl)
+  console.log("papito dos")
+  const domHelper = DOMHelper(browserSession.page, browserSession.window);
   domHelper.printAppTitle();
   const mainElements: MainElements = await domHelper.retrieveElements();
-  if (close_browser) {
-    await browser.close();
-  }
   return mainElements;
 }
 
-async function test(req: Request, res: Response) {
-  for (const website of websitesToTest) {
-    await scrapMainElements(website);
-  }
-  res.status(200).send('Testing done');
+async function getPageContext(website_url: string, browserSession: any) {
+//   const { page, browser, statusCode, window } = await initiatePage(website_url);
+//   if (statusCode !== 200) return false;
+  const domHelper = DOMHelper(browserSession.page, browserSession.window);
+  const context = await domHelper.getPageContext()
+  console.log("context! ", context)
+  return context;
 }
 
 async function findOrCreateProject(website_url: string) {
@@ -55,8 +55,7 @@ async function findOrCreateProject(website_url: string) {
       domain
     }
   });
-
-    
+  console.log("compleamos findOrCreateProject")
   return project[0];
 }
 
@@ -115,37 +114,7 @@ async function createExperiments(elements, journeyId) {
   }
 }
 
-
-
-
-async function onboardNewPage(req: Request, res: Response): Promise<void> {
-  // const prueba = await generateAiTextResponse({
-  //   prompt: "decime algo de 5 palabras, cualquier cosa"
-  // });
-  // console.log("prueba res: ", JSON.stringify(prueba))
-  // return
-  const { website_url } = req.body;
-  const project = await findOrCreateProject(website_url);
-  const mainElements = await scrapMainElements(website_url, false);
-  const createdElements = await createElements(
-    Object.entries(mainElements).map((element) => ({
-      domReference: element[1][0],
-      selector: element[1][1],
-      type: element[0],
-      style: element[1][2],
-    })),
-    project.id,
-  );
-  const journey = await db.Journey.create({
-    name: 'hardcoded name',
-    page: website_url, // Maybe store just the path instead
-    project_id: project.id,
-  });
-  await createExperiments(createdElements, journey.id);
-  res.status(200).send(project);
-}
-
-async function createElements(elements, projectId) {
+async function createElements(elements, journeyId) {
   const createdElements = [];
   for (const element of elements) {
     const createdElement = await db.Element.create({
@@ -156,22 +125,49 @@ async function createElements(elements, projectId) {
         color: await element.domReference.evaluate((el:any) =>  el.style.color), 
         ...element.style
       },
-      project_id: projectId
+      journey_id: journeyId
     });
     createdElements.push(createdElement);
   }
   return createdElements;
 }
 
+async function onboardNewPage(req: Request, res: Response): Promise<void> {
+    console.log("papito!")
+    const { website_url } = req.body;
+    const project = await findOrCreateProject(website_url);
+    console.log("breadcrumbb one!")
+    const browserSession = await initiatePage(website_url);
+    const mainElements = await scrapMainElements(website_url, browserSession);
+    console.log("breadcrumbb two!")
+  
+    const context = await getPageContext(website_url, browserSession);
+  
+    console.log("context to insert: ", context)
+  
+    const journey = await db.Journey.create({
+      name: 'hardcoded name',
+      page: website_url, // Maybe store just the path instead
+      project_id: project.id,
+      context
+    });
+  
+    console.log("journey created man")
+  
+    // elements should have journey_id instead of project_id
+    const createdElements = await createElements(
+      Object.entries(mainElements).map((element) => ({
+        domReference: element[1][0],
+        selector: element[1][1],
+        type: element[0],
+        style: element[1][2],
+      })),
+      journey.id,
+    );
+  
+    await createExperiments(createdElements, journey.id);
+    browserSession.browser.close();
+    res.status(200).send(project);
+  }
 
-export default {
-  scrapMainElements,
-  test,
-  onboardNewPage,
-};
-
-// TODO-P1 create endpoint GET /journey/:id/ to get all the data for a journey
-// TODO-P1 have an actual AI model come up with the text variants
-// TODO-P1 detect the main asset of the page (image, video, etc) and create a variant for that
-// TODO-P1 create endpoint that takes in an analytics account id and returns all the configured events and goals for that account (?) 
-// TODO-P2 - detectar imagen en important area mas grande de X dimensiones (?) 'mainAsset'
+export default onboardNewPage;
