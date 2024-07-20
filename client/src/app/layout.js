04dcda9737f8fef0cdf319dbd41f047e1d9bd4d6
Juan Chaher <juanchaher99@gmail.com>
2024-07-19 21:16:07 -0300

    add gtm tag

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -6,6 +6,7 @@ import getAuthTokenName from './helpers/getAuthTokenName';
 import { cookies } from 'next/headers';
 import styles from './layout.module.css';
 import Head from 'next/head';
+import { GoogleTagManager } from '@next/third-parties/google';
 import Script from 'next/script';
 
 import './globals.css';
@@ -37,8 +38,33 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-HLTERT31Y6');
           `}
         </Script>
+        <Script>
+          {`
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5XVLW5Z9');</script>
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} ${styles.layout}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5XVLW5Z9"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5XVLW5Z9"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <Providers>
           <Nav token={nextAuthSessionToken?.value} />
           <div className={styles.pageContent}>{children}</div>
