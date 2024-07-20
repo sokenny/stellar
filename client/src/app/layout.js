65a8e97ed29cfd8c37db6c98d7dbc67354316ba7
Juan Chaher <juanchaher99@gmail.com>
2024-07-19 21:25:17 -0300

    add gtm tag

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -38,31 +38,15 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-HLTERT31Y6');
           `}
         </Script>
-        <Script>
-          {`
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5XVLW5Z9');</script>
-          `}
-        </Script>
       </head>
+      <GoogleTagManager gtmId="GTM-5XVLW5Z9" />
       <body className={`${inter.className} ${styles.layout}`}>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5XVLW5Z9"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5XVLW5Z9"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
         <Providers>
