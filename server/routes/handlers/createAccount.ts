a6e3dc9815cdd15b1a2a694eda374d2363000793
Juan Chaher <48157442+sokenny@users.noreply.github.com>
2024-10-24 11:48:18 -0300

    add some todos

--- server/routes/handlers/createAccount.ts
+++ server/routes/handlers/createAccount.ts
@@ -43,8 +43,8 @@ async function sendWelcomeEmail(newUser) {
     <p>Welcome to Stellar AB Testing${
       newUser.first_name ? `, ${newUser.first_name}` : ''
     }!</p>
-    <p>You can now start increasing your conversion rates by creating new experiments with out platform.</p>
-    <p>Don't have a clear idea on what to test? Start by making text variations of your main elements! Such as your h1, CTA, etc. Plenty of CR potential is usually locked inside simple tweaks of this nature.</p>
+    <p>You can now start increasing your conversion rates by creating new experiments with our platform.</p>
+    <p>Don't have a clear idea on what to test? Start by making text variations of your main elements! Such as your h1 or CTA. Plenty of CR potential is usually locked inside simple tweaks of this nature.</p>
     <p>Feel free to reach to us at <b>hello@gostellar.app</b> if you need any help getting set up :)</p>
   `;
 
