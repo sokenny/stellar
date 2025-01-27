1f155d5c673b64ef40ec5925880a543e68b7b6e3
Juan Chaher <48157442+sokenny@users.noreply.github.com>
2025-01-27 00:51:25 -0300

    override sopme nextui shit

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 
 // Rough estimate of events processed per monthL: Per day we can do 172,800 with 1 worker. LEts cut out 20% of that to be conservative, so 138,240. PEr month that is 4,147,200.
 
-// TODO-p1-1: write migration to create goals_experiments for all goals with an experiment_id -
+// TODO-p1-1: Improve "Which URL is the element at?" input
 // TODO-p1-1: Create backdoor to flush redis
 // TODO-p1-1: Tidy up auto-name generation for experiments
 // TODO-p1-1: In edit variant modal, have focus of attention placed in the edited variant fields
